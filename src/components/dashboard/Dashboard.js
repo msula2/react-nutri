f7a3054127a92f2e0f2394f81dc117222eeb7325
msula2 <msula2@uis.edu>
2024-05-03 21:42:15 +0500

    Add line chart to dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Navigate } from 'react-router-dom';
-import { Alert} from "@blueprintjs/core";
+import { Alert, Card, H5, Button } from "@blueprintjs/core";
+import Loader from '../loader/Loader';
+import Title from '../title/Title';
+import About from '../about/About';
+import MealLineChart from './MealLineChart';
 
 class Dashboard extends Component {
     constructor(props) {
@@ -9,6 +13,19 @@ class Dashboard extends Component {
         user: {
           name: '',
           id: ''
+        },
+        diet: {
+          goal: {
+            tdee: '',
+            action: '',
+            activity: '',
+            bmr: ''
+          },
+          current: {
+            tdee: '',
+            meals: []
+          }
+        
         },
         loggedIn : null,
         timedOut: null,
@@ -29,6 +46,21 @@ class Dashboard extends Component {
   
       this.setState({ user: { name: '', id: '' }, loggedIn: false, timedOut: true});
       };
+
+      fetchDietDetails = (id) => {
+        if (id != undefined){
+            fetch(`${process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_URL : process.env.REACT_APP_DEPLOYED_URL}/user/${id}/diet/details`, {
+              method: 'get',
+              headers: {'Content-Type': 'application/json'},
+                credentials: 'include'
+            })
+            .then(response => response.json())
+            .then(data => {
+              this.setState({diet: data.diet})
+            });
+        }
+        
+      }
   
       checkSession = () => {
       fetch(`${process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_URL : process.env.REACT_APP_DEPLOYED_URL}/user`, {
@@ -40,6 +72,8 @@ class Dashboard extends Component {
       .then(data => {
           if (data.loggedIn === true){
           this.setUserDetails(data.user.username, data.user.id, data.loggedIn);
+          this.fetchDietDetails(data.user.id);
+
           } else if (data.loggedIn === false){
           this.clearSession();
           } else {
@@ -47,12 +81,14 @@ class Dashboard extends Component {
           }
       });
       };
+
   
       componentDidMount() {
           setTimeout(() => {
               this.setState({ loading: false });
           }, 3000);
           this.checkSession();
+          this.fetchDietDetails();
           
       }
   
@@ -75,8 +111,20 @@ class Dashboard extends Component {
   
     render() {
 
-      const {user, timedOut, alert} = this.state;
+      const {user, timedOut, loading, message, alert} = this.state;
+      const {diet} = this.state;
+      const { goal, current } = diet;
 
+      const mealData = current.meals.map(meal => ({
+        name: meal.name,
+        calories: parseInt(meal.total_calories) 
+      }));
+      const goalTdee = parseInt(goal.tdee); 
+      
+      let action = diet.goal.action;
+      let eat = diet.goal.action == "gain" ? "more" : "less";
+      let current_tdee = diet.current.tdee 
+      let goal_tdee = diet.goal.tdee;
       return (
         <div>
           {timedOut ? 
@@ -96,11 +144,52 @@ class Dashboard extends Component {
             )
             :
             (
-              <h1 className='white'>Welcome {user.name}</h1>
+              <>
+              {loading && (
+                <div className="loader-overlay">
+                    <Loader message={message} />
+                </div>
+              )}
+              <div className='vw-100 vh-100 d-flex flex-column justify-center items-center' style={{ display: loading ? 'none' : '' }}>
+              <Title text={`${user.name}'s Dashboard`} color="#FFE39F" />
+              <About text={`The dashboard allows you to quickly view your calories for the day. Given that you aim to ${action} weight, you should aim to consume ${eat} than the calories mentioned below`} />
+              <div className="flex align-items justify-between ml5 ph4-ns">
+                <table className="mt2" style={{ border: '2px solid #a6d940', borderRadius: '5px', padding: '0.5rem' }}>
+                  <caption className="tc"><h2 className="f3">Calories consumed today</h2></caption>
+                  <thead>
+                    <tr>
+                      <th className="tc"><h2 className="f3">Current</h2></th>
+                      <th className="tc"><h2 className="f3">Goal</h2></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="tc f1">
+                        <h1 className={`dashboard mt1 ${action === "gain" ? parseFloat(current_tdee) < parseFloat(goal_tdee) ? "dark-red" : "green" : parseFloat(current_tdee) > parseFloat(goal_tdee) ? "dark-red" : "green"}`}>
+                          {current_tdee}
+                        </h1>
+                      </td>
+                      <td className="tc f1"><h1 className="mt1">{goal_tdee}</h1></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="w-60 mt5">
+                <div>
+                  <MealLineChart mealData={mealData} goalTdee={goalTdee} />
+                </div>
+
+                </div>
+              </div>
+            </div>
+
+              </>
+              
             )
           }
         </div>
       );
+        
+      
     }
   }
   export default Dashboard;
